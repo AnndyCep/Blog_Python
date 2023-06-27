5bc8395af3a7e42196c6a6d0c7f19aa31d0edfb1
AnndyCep <hannsonandrey@hotmail.com>
2023-06-26 22:04:42 -0500

    Creo el modelo, como formulario GET POST.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+   
+
+# every time we make modification to the models, we make migrations here.
+
